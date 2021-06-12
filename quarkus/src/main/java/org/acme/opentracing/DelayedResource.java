0f3bd7b704fb743a6dc5f72bbd190d16296e15ea
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2021-06-12 22:40:18 +0200

    sustentacion: agregado un endpoint para ilustrar resteasy manipulando el hilo, actualizadas dependencias para quarkus 2-beta

---
+++ quarkus/src/main/java/org/acme/opentracing/DelayedResource.java
@@ -0,0 +1,44 @@
+package org.acme.opentracing;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.CompletionStageRxInvoker;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+@Path("")
+public class DelayedResource {
+
+    public static final int AMOUNT_CALLS = 1000;
+    public static final int DURATION_MS = 1000;
+
+    private final AtomicInteger someCounter = new AtomicInteger();
+
+    @Path("/concurrent-nap")
+    @GET
+    public CompletionStage<String> concurrentNap(@Context UriInfo uriInfo) {
+        URI uri = uriInfo.getBaseUriBuilder().path(DelayedResource.class, "delayed").build();
+        WebTarget target = ClientBuilder.newClient().target(uri);
+        CompletionStageRxInvoker invocation = target.request().rx();
+        StringBuilder sb = new StringBuilder();
+        CompletableFuture<String>[] calls = new CompletableFuture[AMOUNT_CALLS];
+        IntStream.range(0, AMOUNT_CALLS).forEach(i -> calls[i] = invocation.get(String.class)
+                .whenComplete((val, t) -> sb.append(val).append(", "))
+                .toCompletableFuture());
+        return CompletableFuture.allOf(calls).thenApply(v -> sb.toString());
+    }
+
+    @Path("delayed")
+    @GET
+    public int delayed() throws InterruptedException {
+        Thread.sleep(DURATION_MS);
+        return someCounter.getAndIncrement();
+    }
+}

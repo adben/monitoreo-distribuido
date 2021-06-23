ecaab5009e2c268959180fad6f630f6608ae35cd
Adolfo Benedetti <adolfo.benedetti@gmail.com>
2021-06-23 19:32:00 +0200

    sustentacion: actualizada version de quarkus, arreglados warnings acerca de ReST

--- quarkus/src/main/java/org/acme/opentracing/DelayedResource.java
+++ quarkus/src/main/java/org/acme/opentracing/DelayedResource.java
@@ -2,10 +2,12 @@ package org.acme.opentracing;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.CompletionStageRxInvoker;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
 import java.net.URI;
 import java.util.concurrent.CompletableFuture;
@@ -18,11 +20,11 @@ public class DelayedResource {
 
     public static final int AMOUNT_CALLS = 1000;
     public static final int DURATION_MS = 1000;
-
     private final AtomicInteger someCounter = new AtomicInteger();
 
-    @Path("/concurrent-nap")
     @GET
+    @Path("/concurrent-nap")
+    @Produces(MediaType.TEXT_PLAIN)
     public CompletionStage<String> concurrentNap(@Context UriInfo uriInfo) {
         URI uri = uriInfo.getBaseUriBuilder().path(DelayedResource.class, "delayed").build();
         WebTarget target = ClientBuilder.newClient().target(uri);
@@ -35,8 +37,9 @@ public class DelayedResource {
         return CompletableFuture.allOf(calls).thenApply(v -> sb.toString());
     }
 
-    @Path("delayed")
     @GET
+    @Path("delayed")
+    @Produces(MediaType.TEXT_PLAIN)
     public int delayed() throws InterruptedException {
         Thread.sleep(DURATION_MS);
         return someCounter.getAndIncrement();
